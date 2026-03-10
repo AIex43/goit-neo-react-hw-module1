11a3c71f3bbde0e0b49ab95b9b4c514347c65789
AIex43 <alex2612061@gmail.com>
2026-03-10 18:27:53 +0300

    f

---
+++ ASDF/src/components/FriendListItem/FriendListItem.jsx
@@ -0,0 +1,14 @@
+import styles from "./FriendListItem.module.css";
+
+export default function FriendListItem({ avatar, name, isOnline }) {
+  return (
+    <div className={styles.card}>
+      <img src={avatar} alt="Avatar" width="48" />
+      <p>{name}</p>
+
+      <p className={isOnline ? styles.online : styles.offline}>
+        {isOnline ? "Online" : "Offline"}
+      </p>
+    </div>
+  );
+}
